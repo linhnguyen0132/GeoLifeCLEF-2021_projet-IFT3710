e9ff461268e48d9691875049fa9ef2fcf3501cc2
Daniela <168323446+danielaDaliesio@users.noreply.github.com>
2026-04-30 18:15:00 -0400

    Données utilisées obs_and_patches.py
    Enlever données US comme pour 2022

--- dani_test/obs_and_patches.py
+++ dani_test/obs_and_patches.py
@@ -1,11 +1,4 @@
-import torch
-from torch.utils.data import Dataset
-from pathlib import Path
-import pandas as pd
-from PIL import Image
-import numpy as np
-
-class GeoLifeDataset(Dataset):
+class GeoLifeDataset2(Dataset):
 
     def __init__(self, root, subset="train", transform=None):
 
@@ -15,24 +8,20 @@ class GeoLifeDataset(Dataset):
         print("Loading observations...")
 
         df_fr = pd.read_csv(self.root / "observations" / "observations_fr_train.csv", sep=";")
+        #df_us = pd.read_csv(self.root / "observations" / "observations_us_train.csv", sep=";")
 
-        # Charger les données US
-        df_us = pd.read_csv(self.root / "observations" / "observations_us_train.csv", sep=";")
+        df = pd.concat([df_fr])
 
-        # Mettre US et France en un seul dataframe
-        df = pd.concat([df_fr, df_us])
-
-        # garder seulement les espèces avec >= 5 images
-        valid_species = species_counts[species_counts >= 20].index
-
-        # filtrer le dataframe
+        # keep only species with >=20 observations
+        species_counts = df["species_id"].value_counts()
+        valid_species = species_counts[species_counts >= 1].index
         df = df[df["species_id"].isin(valid_species)].reset_index(drop=True)
 
-        # Créer un mapping des species label
+        # label mapping
         species_ids = sorted(df["species_id"].unique())
         self.label_map = {species_id: i for i, species_id in enumerate(species_ids)}
 
-        # Split en train et validation
+        # train / validation split
         df = df[df["subset"] == subset]
 
         patches_dir = self.root / "patches_sample"
@@ -41,12 +30,14 @@ class GeoLifeDataset(Dataset):
 
         print("Building dataset...")
 
-        for row in df.itertuples():  
+        for row in df.itertuples():
 
             obs_id = row.observation_id
             species = row.species_id
+            lat = row.latitude
+            lon = row.longitude
 
-            region = "fr" if obs_id < 20000000 else "us"
+            region = "fr" 
 
             sub1 = str(obs_id)[-2:]
             sub2 = str(obs_id)[-4:-2]
@@ -54,30 +45,27 @@ class GeoLifeDataset(Dataset):
             img_path = patches_dir / region / sub1 / sub2 / f"{obs_id}_rgb.jpg"
 
             if img_path.exists():
-                self.observations.append((img_path, species))
+                self.observations.append((img_path, species, lat, lon))
 
-        print("Images trouvées :", len(self.observations))
+        print("Images found:", len(self.observations))
 
     def __len__(self):
         return len(self.observations)
 
     def __getitem__(self, idx):
 
-        img_path, species = self.observations[idx]
+        img_path, species, lat, lon = self.observations[idx]
 
         obs_id = img_path.stem.replace("_rgb", "")
         nir_path = img_path.with_name(f"{obs_id}_near_ir.jpg")
 
-        # charger RGB
         rgb = Image.open(img_path).convert("RGB")
 
-        # charger NIR (grayscale)
         if nir_path.exists():
             nir = Image.open(nir_path).convert("L")
         else:
-            print("NIR not found!")
-        
-        # convertir en numpy
+            nir = Image.new("L", rgb.size)
+
         rgb = np.array(rgb)
         nir = np.array(nir)
 
@@ -90,6 +78,9 @@ class GeoLifeDataset(Dataset):
         if self.transform:
             image = self.transform(image)
 
+        # normalize coordinates
+        coords = torch.tensor([lat/90, lon/180], dtype=torch.float32)
+
         label = self.label_map[species]
 
-        return image, label
+        return image, coords, label
